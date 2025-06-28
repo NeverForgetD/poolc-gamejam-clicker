0a8db2bd09aa3d2dc38a22715bd97aeaf21440f5
Jongyup Kim <0327max@gmail.com>
2025-06-28 14:32:39 +0900

    all in 1 sprtie renderer 추가

--- Clicker/Assets/01.Script/Effect/ChickenFlap.cs
+++ Clicker/Assets/01.Script/Effect/ChickenFlap.cs
@@ -1,26 +1,27 @@
 using UnityEngine;
-using UnityEngine.UI;
 using DG.Tweening;
-using Unity.VisualScripting;
 
 /// <summary>
-/// 클릭 시 닭이 파닥이는 애니메이션
+/// SpriteRenderer 기반 닭 파닥 애니메이션
 /// </summary>
 public class ChickenFlap : MonoBehaviour
 {
-    private RectTransform rt;
+    private Transform t;
 
     void Awake()
     {
-        rt = GetComponent<RectTransform>();
+        t = GetComponent<Transform>();
     }
 
+    /// <summary>
+    /// 클릭 시 파닥거림 효과
+    /// </summary>
     public void Flap()
     {
-        DG.Tweening.Sequence flapSeq = DOTween.Sequence();
+        Sequence flapSeq = DOTween.Sequence();
 
-        // 작아졌다가 커지는 Scale 연출
-        flapSeq.Append(rt.DOScale(new Vector3(0.8f, 1.2f, 1), 0.1f))
-               .Append(rt.DOScale(Vector3.one, 0.1f));
+        // 작아졌다 커지는 Scale 연출
+        flapSeq.Append(t.DOScale(new Vector3(0.8f, 1.2f, 1), 0.1f))
+               .Append(t.DOScale(Vector3.one, 0.1f));
     }
 }
